c7b4f6e6f0cfc3cb14ac96c0b08063a4c47c1333
Silvia Abe <silviaabebuzatto@gmail.com>
2025-06-02 13:25:13 -0300

    install Storybook

--- eslint.config.js
+++ eslint.config.js
@@ -11,41 +11,45 @@ import tseslint from "typescript-eslint";
 import prettierPlugin from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
 
-export default tseslint.config({ ignores: ["dist"] }, {
-  extends: [
-    js.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    prettierConfig,
-  ],
-  files: ["**/*.{ts,tsx}"],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: {
-      ...globals.browser,
-    },
-    parserOptions: {
-      project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-      tsconfigRootDir: import.meta.dirname,
-      ecmaFeatures: {
-        jsx: true,
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      prettierConfig,
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "react-x": reactX,
+      "react-dom": reactDom,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...reactX.configs["recommended-typescript"].rules,
+      ...reactDom.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "prettier/prettier": "warn",
+    },
   },
-  plugins: {
-    "react-hooks": reactHooks,
-    "react-refresh": reactRefresh,
-    "react-x": reactX,
-    "react-dom": reactDom,
-    prettier: prettierPlugin,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    ...reactX.configs["recommended-typescript"].rules,
-    ...reactDom.configs.recommended.rules,
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "prettier/prettier": "warn",
-  },
-}, storybook.configs["flat/recommended"]);
+  storybook.configs["flat/recommended"],
+);
