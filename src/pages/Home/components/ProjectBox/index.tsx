dba0df257eda4647a01f8c5d66582c5702c7868c
Silvia Abe <silviaabebuzatto@gmail.com>
2025-06-12 16:00:18 -0300

    ANJO-7: Projects That Move Us Section

---
+++ src/pages/Home/components/ProjectBox/index.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const ProjectBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: auto;
+  border-radius: 12px;
+  padding: 18px;
+  gap: 18px;
+  background-color: var(--mui-palette-secondary-main);
+`;
+
+export default ProjectBox;
---
+++ src/pages/Home/components/ProjectInfo/index.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const ProjectInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+  color: var(--mui-palette-title-main);
+  background-color: var(--mui-palette-neutral-100);
+  width: 100%;
+  height: auto;
+  border-radius: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 18px;
+`;
+
+export default ProjectInfo;
