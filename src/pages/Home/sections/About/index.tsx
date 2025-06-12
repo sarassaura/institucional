a7e9db2bab08039ecdd50bf6a22045bfb594f3db
Silvia Abe <silviaabebuzatto@gmail.com>
2025-06-12 15:58:31 -0300

    ANJO-5: About Section

---
+++ src/pages/Home/sections/About/index.tsx
@@ -0,0 +1,29 @@
+import type { ReactNode } from "react";
+import Title from "../../components/Title";
+import Container from "../../components/Container";
+import SubTitle from "../../components/Subtitle";
+import videoInstitucional from "../../../../assets/images/imagem-instucional.png";
+import { Box } from "@mui/material";
+
+function About(): ReactNode {
+  return (
+    <Container gap="32px">
+      <Title variant="h2">Sobre a Bike Anjo</Title>
+      <SubTitle>
+        A Bike Anjo é uma rede de voluntáries que ensina pessoas a pedalar,
+        indica rotas seguras e acompanha seus primeiros trajetos. Acreditamos na
+        bicicleta como ferramenta de transformação para pessoas e cidades mais
+        humanas.
+      </SubTitle>
+      <Box
+        sx={{
+          marginTop: "8px",
+        }}
+      >
+        <img src={videoInstitucional} alt="Vídeo Institucional" />
+      </Box>
+    </Container>
+  );
+}
+
+export default About;
