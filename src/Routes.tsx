83182655651476d3f00444fa9ee0312c70ad118e
mariagmss <166930239+mariagmss@users.noreply.github.com>
2025-06-12 22:26:17 -0300

    adjust routing structure to use Layout wrapper

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import Layout from "./pages/Layout/layout";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </>
   );
